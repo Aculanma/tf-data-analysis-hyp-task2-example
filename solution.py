812040a148b914a18969c4ca554e2f7efa5eb497
Ildus Iskhakov <108624071+Aculanma@users.noreply.github.com>
2023-04-10 21:57:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1099254596 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.09
+    p_val = stats.ks_2samp(x, y)[1]
+    return True if res <= alpha else False # Ваш ответ, True или False
